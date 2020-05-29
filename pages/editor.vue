e4bc5bdf5e4cfd41c7e615158a81159d29a11766
Ian <ianx021@gmail.com>
2020-05-29 13:31:04 -0300

    allow selection of substrings & display json data

--- pages/editor.vue
+++ pages/editor.vue
@@ -7,7 +7,7 @@
 
       <div class="editor computed-html" v-html="computedHtml" />
       <div class="editor">
-        JSON data
+        {{ showJsonList }}
       </div>
       <div class="links">
         <nuxt-link
@@ -32,7 +32,9 @@ export default {
       start_index: null,
       end_index: null,
       substring: '',
-      annotation: ''
+      annotation: '',
+      jsonList: {},
+      showJsonList: {}
     }
   },
 
@@ -60,13 +62,63 @@ export default {
       const doc = document.getElementsByClassName('character-span')
       for (let i = 0; i < doc.length; i++) {
         doc[i].id = i
-        doc[i].addEventListener('click', () => { this.clicked(doc[i]) })
+        doc[i].addEventListener('click', () => { this.clicked(i, doc[i]) })
       }
     }
   },
   methods: {
-    clicked (i) {
-      i.style.color = 'red'
+    clicked (i, doc) {
+      doc.style.color = 'red'
+      this.computeIndex(i)
+      this.generateSubstring()
+      this.generateJson()
+      this.updateShowJsonList()
+    },
+    computeIndex (i) {
+      if (this.end_index === null) {
+        if (this.start_index === null) {
+          this.start_index = i
+        } else if (this.start_index !== null && i > this.start_index) {
+          this.end_index = i
+        } else if (this.start_index !== null && i < this.start_index) {
+          this.end_index = this.start_index
+          this.start_index = i
+        }
+      }
+    },
+    generateSubstring () {
+      if (this.start_index !== null && this.end_index !== null) {
+        const doc = document.getElementsByClassName('character-span')
+        const charList = []
+        for (let i = this.start_index; i < this.end_index + 1; i++) {
+          doc[i].style.backgroundColor = 'yellow'
+          // doc[i].addEventListener('click', () => { this.clicked(i, doc[i]) })
+          charList.push(doc[i].innerHTML)
+        }
+        this.substring = charList.join('')
+      }
+    },
+    generateJson () {
+      if (this.start_index !== null && this.end_index !== null && this.substring) {
+        const id = this.start_index * 100000 + this.end_index
+        this.jsonList[id] = {
+          id,
+          start_index: this.start_index,
+          end_index: this.end_index,
+          substring: this.substring,
+          annotation: this.annotation
+        }
+        this.resetJsonData()
+      }
+    },
+    resetJsonData () {
+      this.start_index = null
+      this.end_index = null
+      this.substring = ''
+      this.annotation = ''
+    },
+    updateShowJsonList () {
+      this.showJsonList = JSON.stringify(this.jsonList)
     }
   }
 }
