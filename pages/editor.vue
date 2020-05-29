e5efab790ed723bf628ab217eb7b3d09e44d5126
Ian <ianx021@gmail.com>
2020-05-28 22:32:50 -0300

    parsing text into spans

--- pages/editor.vue
+++ pages/editor.vue
@@ -4,13 +4,8 @@
       <div class="instructions">
         Crtl + Click on the beggining and the end of the text you want to mark Click on the marked text to add an annotation
       </div>
-      <b-form-textarea
-        id="textarea"
-        v-model="text"
-        placeholder="Enter something..."
-        rows="20"
-        readonly=""
-      />
+
+      <div class="editor computed-html" v-html="computedHtml" />
       <div class="editor">
         JSON data
       </div>
@@ -33,7 +28,45 @@
 export default {
   data () {
     return {
-      text: this.$route.params.text
+      text: this.$route.params.text,
+      start_index: null,
+      end_index: null,
+      substring: '',
+      annotation: ''
+    }
+  },
+
+  computed: {
+    computedHtml () {
+      if (this.text) {
+        const textBroken = this.text.split('\n')
+        const htmlBroken = Array(textBroken.length).fill('')
+        for (let line = 0; line < textBroken.length; line++) {
+          for (let k = 0; k < textBroken[line].length; k++) {
+            htmlBroken[line] += `<span class="character-span">${textBroken[line].charAt(k)}</span>`
+          }
+        }
+        return htmlBroken.join('<br/>')
+      } else {
+        return ''
+      }
+    }
+  },
+
+  mounted () {
+    if (!this.text) {
+      this.$router.push('/')
+    } else {
+      const doc = document.getElementsByClassName('character-span')
+      for (let i = 0; i < doc.length; i++) {
+        doc[i].id = i
+        doc[i].addEventListener('click', () => { this.clicked(doc[i]) })
+      }
+    }
+  },
+  methods: {
+    clicked (i) {
+      i.style.color = 'red'
     }
   }
 }
@@ -41,6 +74,7 @@ export default {
 
 <style>
 .editor {
+  text-align: justify;
   background-color: lightgray;
   border-radius: 5px;
   padding:15px;
